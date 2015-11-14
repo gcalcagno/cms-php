078fce072963605aa3a4fe2d015afd3168d5247c
Giannina <calcagno.giannina@hotmail.com>
2015-11-14 15:09:41 -0300

    testeando error online

--- models/config/FuncionesConfig.php
+++ models/config/FuncionesConfig.php
@@ -10,11 +10,11 @@ class FuncionesConfig
     */
     public function limitarTextos($valor, $limite)
     {
-        echo mb_substr($valor, 0, $limite, 'UTF-8');
+       /* echo mb_substr($valor, 0, $limite, 'UTF-8');
         $logn = mb_strlen($valor, 'UTF-8');
         if($logn >= $limite){
             echo ' ...';
-        }
+        }*/
     }
 
 }
