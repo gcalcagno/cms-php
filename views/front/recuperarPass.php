a96df3f7f6ce9f10cd67b269dc071fd23a0605f8
Giannina <calcagno.giannina@hotmail.com>
2015-11-18 00:30:44 -0300

    ajuste recuperar contraseña oculto

--- views/front/recuperarPass.php
+++ views/front/recuperarPass.php
@@ -20,14 +20,15 @@
         <div class="subtitulo titulo-naranja"> 
             <h3>Recuperar Password</h3>
         </div>
-        <form role="form" action="" method="POST" class="col-lg-6 col-md-6 col-sm-12 ">
+        <p>Para recuperar tu contraseña contactate con <strong>info@calcagnogiannina.com.ar</strong></p>
+        <!--<form role="form" action="" method="POST" class="col-lg-6 col-md-6 col-sm-12 ">
           <div class="form-group">
             <label for="email">Email</label>
             <input type="email" class="form-control" name="email"
-            value="<?php if (isset($_POST['email'])){echo $_POST['email']; }?>">
+            value="<?php// if (isset($_POST['email'])){echo $_POST['email']; }?>">
           </div>
           <button type="submit" name="recuperar" value="enviar" class="btn enviar btn-default text-uppercase">Enviar</button>
-        </form>
+        </form>-->
 
     </div>
     <!--*** //FORM REGISTRO ***-->
